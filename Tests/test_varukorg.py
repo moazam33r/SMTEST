a2ff6df18101ffc78c5360721050d352f4dc9059
sadaqkeynaan <maxsad3741@gmail.com>
2026-03-10 10:46:46 +0100

    varukorg

--- Tests/test_varukorg.py
+++ Tests/test_varukorg.py
@@ -1,5 +1,9 @@
-from playwright.sync_api import sync_playwright
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from funktion import accept_cookies, search_product, select_product, add_to_cart
+from playwright.sync_api import sync_playwright
 
 def test_lagg_i_varukorg():
     with sync_playwright() as p:
@@ -10,14 +14,16 @@ def test_lagg_i_varukorg():
         # Gå till webbshopen
         page.goto("https://www.inet.se/")
         
-        # Anropa funktionerna från test_helpers
+        # Anropa funktionerna
         accept_cookies(page)
         search_product(page, "Samsung Galaxy S25")
         select_product(page, "Samsung Galaxy S25 Ultra Silicone Case Grå")
         add_to_cart(page)
         
-        # Vänta så du ser resultatet
+        # Vänta 3 sekunder så du ser resultatet
         page.wait_for_timeout(3000)
+        
+        # Stäng webbläsaren
         browser.close()
 
 if __name__ == "__main__":
